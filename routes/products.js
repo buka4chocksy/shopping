75387a1fb6ce1e1a4b98c0969d46febfe1fb18ee
buka4chocksy <buka4chocksy@gmail.com>
2018-07-28 21:30:55 -0700

    added multer and an upload function so as to upload files and text

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
-
 var productsControllers = require('../controllers/productControllers');
+var multer = require('multer');
+var product = require('../models/product');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null , './uploads')
+
+    },
+    filename: function(req,file, cb){
+        cb(null ,file.originalname)
+
+    }
+})
+//the limts aids in determining the limits of file size you can upload
+var upload = multer({storage:storage , limits:{
+
+}});
+
  
-router.post('/create', productsControllers.addProduct);
+router.post('/create', upload.single('image'), function(req,res){
+    console.log(req.file);
+   
+    var data ={
+        productname:req.body.productname,
+        price:req.body.price,
+        quantity:req.body.quantity,
+        image:req.file.path,
+        time:Date.now()
+
+    }
+    product.create(data , function(err){
+        if(err) res.json({err:err,message:'Error Adding Products'});
+        res.json({message:'Product Added Successfully'});
+
+    });
+
+
+});
 router.post('/delete',productsControllers.deleteProduct);
 router.post('/update', productsControllers.updateProduct);
 router.get('/',productsControllers.viewProducts);
