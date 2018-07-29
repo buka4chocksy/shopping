535f79fbc116369b9401159d09f9a7b1b5ccb35b
buka4chocksy <buka4chocksy@gmail.com>
2018-07-28 21:28:50 -0700

    altered the product model

--- models/product.js
+++ models/product.js
@@ -4,6 +4,7 @@ var productSchema = mongoose.Schema({
     productname:{ type : String, required : true},
     price:{type:Number, required:true},
     quantity:{type:Number, required:true},
+    image:String,
     time:Date
 
 })
