46993a225068f552064b92525f4ec5981277353f
buka4chocksy <buka4chocksy@gmail.com>
2018-07-28 21:29:46 -0700

    added the view order router in the  orders routes

--- routes/orders.js
+++ routes/orders.js
@@ -5,5 +5,6 @@ var orderControllers = require('../controllers/orderControllers');
 
 router.post('/order',orderControllers.orderProduct);
 router.get('/:id', orderControllers.deleteOrder);
+router.get('/', orderControllers.viewOrder);
 
 module.exports = router;
